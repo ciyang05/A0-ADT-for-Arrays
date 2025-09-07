7d30bd6c98baa93450559985600490bc9be537e2
cyang-05 <chiashiiyang@gmail.com>
2025-09-07 14:49:32 -0400

    first method

--- IndexAccessADT.java
+++ IndexAccessADT.java
@@ -1,4 +1,12 @@
 /** interface to describe operations supported by built-in array */
 interface IndexAccessADT {
     
+    /**
+     * 
+     * @param num
+     * returns element at accessed index
+     */
+    void accessIndex(int num);
+
+    
 }
