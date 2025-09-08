8e87c497cc72605dd1ee3642553f6925ff895755
kiaras-create <ksunilsingh@smith.edu>
2025-09-08 16:06:30 -0400

    Sequence methods

--- SequenceADT.java
+++ SequenceADT.java
@@ -3,4 +3,39 @@
  */
 interface SequenceADT {
     
+    
+    /**
+     * accesses and returns the array
+     */
+    public void getArray();
+
+
+    /**
+     * 
+     * @param num
+     * returns element at accessed index
+     * will throw an exception if specified index is out of bounds
+     */
+    public void accessEle(int num);
+
+
+    /**
+     * 
+     * @param num
+     * access and change the element at specified index
+     * will throw an exception if the specified index is out of bounds
+     */
+    public void changeEle(int num);
+
+
+    /**
+     * @param num
+     * sorts the array in whichever order
+     * will throw an exception if the specified index is out of bounds
+     */
+    public void sortArray(int num);
+
+
 }
+
+
